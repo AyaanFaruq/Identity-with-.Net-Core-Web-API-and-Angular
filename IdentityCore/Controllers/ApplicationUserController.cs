c5fc434f4eae535cc22c38031e1e02e018b68d86
AyaanFaruq <ayaanfaruq@gmail.com>
2019-12-11 09:01:51 +0600

    Initial Commit

---
+++ IdentityCore/Controllers/ApplicationUserController.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Amazon.Util.Internal.PlatformServices;
+using IdentityCore.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+
+namespace IdentityCore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApplicationUserController : ControllerBase
+    {
+        private UserManager<ApplicationUser> _userManager;
+        private SignInManager<ApplicationUser> _signInManager;
+        private readonly Models.ApplicationSettings _appSettings;
+
+        public ApplicationUserController(UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signInManager, IOptions<Models.ApplicationSettings> appSettings)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _appSettings = appSettings.Value;
+        }
+
+        [HttpPost]
+        [Route("Register")]
+
+        //POST : /api/ApplicationUser/Register
+        
+        public async Task<Object> PostApplicationUser(ApplicationUserModel appUserModel)
+        {
+            var applicationUser = new ApplicationUser()
+            {
+                UserName = appUserModel.UserName,
+                Email = appUserModel.Email,
+                FullName = appUserModel.FullName
+            };
+            try
+            {
+                var result = await _userManager.CreateAsync(applicationUser, appUserModel.Password);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+
+        }
+
+
+        [HttpPost]
+        [Route("Login")]
+        //POST : /api/ApplicationUser/Login
+        public async Task<IActionResult> Login(LoginModel model)
+        {
+            var user = await _userManager.FindByNameAsync(model.UserName);
+            if (user != null && await _userManager.CheckPasswordAsync(user, model.Password))
+            {
+                var tokenDescriptor = new SecurityTokenDescriptor
+                {
+                    Subject = new ClaimsIdentity(new Claim[]
+                    {
+                        new Claim("UserID",user.Id.ToString())
+                    }),
+                    Expires = DateTime.UtcNow.AddDays(1),
+                    SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_appSettings.JWT_Secret)), SecurityAlgorithms.HmacSha256Signature)
+                };
+                var tokenHandler = new JwtSecurityTokenHandler();
+                var securityToken = tokenHandler.CreateToken(tokenDescriptor);
+                var token = tokenHandler.WriteToken(securityToken);
+                return Ok(new { token });
+            }
+            else
+                return BadRequest(new { message = "Username or password is incorrect." });
+        }
+
+    }
+}
